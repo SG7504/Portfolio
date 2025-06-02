fe198a864adda7166b166f9f9a7dac81c6dd7449
Sparsh <sparshguha7504@gmail.com>
2025-06-02 12:07:29 +0530

    Update Skills.js import case and fix build error

--- src/data/Contact.js
+++ src/data/Contact.js
@@ -1,8 +1,8 @@
-const contact = {
+const Contact = {
   email: "sparshguha4057@gmail.com",
   github: "https://github.com/SG7504",
   linkedin: "https://www.linkedin.com/in/sparshguha75/",
   resume: "https://drive.google.com/uc?export=view&id=1lG9lbAbShlpNkfapearw97irjEgLiWSt"
 };
 
-export default contact;
+export default Contact;
