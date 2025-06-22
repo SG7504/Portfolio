d4967562a5e62288986a4a7002c5c4f26489c16b
Sparsh <sparshguha7504@gmail.com>
2025-06-22 15:47:25 +0530

    change name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Resume/', // <-- Set this to your repo name
+  base: '/Portfolio/', // <-- Set this to your repo name
 });
