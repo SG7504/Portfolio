4c3ef5ef7158a8623e483a41e9f5e4818db9832b
Sparsh Guha <143119599+SG7504@users.noreply.github.com>
2026-01-19 11:45:10 +0530

    Change resume link to relative path
    Updated the resume link to a relative path.

--- src/data/Contact.js
+++ src/data/Contact.js
@@ -2,7 +2,7 @@ const Contact = {
   email: "sparshguha4057@gmail.com",
   github: "https://github.com/SG7504",
   linkedin: "https://www.linkedin.com/in/sparshguha75/",
-  resume: "https://drive.google.com/file/d/1XR5PbxP3lyQAJL_8ZDDJzS1u2bVmSqBt/view?usp=sharing"
+  resume: "/resume.pdf"
 };
 
 export default Contact;
