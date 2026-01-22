51afb630124b46c576f3a55d1b1f0f267055d466
Sparsh Guha <143119599+SG7504@users.noreply.github.com>
2026-01-22 13:14:45 +0530

    Rename CertificationPage to ProjectCard and update imports

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,16 +1,16 @@
-import certifications from "../data/certifications";
-import CertificationList from "../components/CertificationList";
+import certifications from "../data/Projects";
+import CertificationList from "../components/ProjectCard";
 import Layout from "../components/Layout";
 
-export default function CertificationPage() {
+export default function ProjectCard() {
   return (
     <Layout>
       <section className="min-h-screen px-6 py-12 max-w-6xl mx-auto text-white">
         <h2 className="text-4xl font-bold mb-10 text-center">
-          Certifications
+          Projects
         </h2>
 
-        <CertificationList certifications={certifications} />
+        <ProjectCard Projects={Projects} />
       </section>
     </Layout>
   );
