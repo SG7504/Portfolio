195bbe9a54d4c983dd29496f87558a90043ca8be
Sparsh Guha <143119599+SG7504@users.noreply.github.com>
2026-01-24 12:07:20 +0530

    Update Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import education from "../data/Education";
+import education from "../data/Education.js";
 import { FaUniversity, FaChartLine } from "react-icons/fa";
 import { GiGraduateCap } from "react-icons/gi";
 import { LuCalendarDays } from "react-icons/lu";
