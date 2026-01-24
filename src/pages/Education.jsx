9921128f73c9013c9473189d07d34fc9299d7587
Sparsh Guha <143119599+SG7504@users.noreply.github.com>
2026-01-24 11:45:55 +0530

    Update Education.jsx

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,16 +1,7 @@
-import Layout from "../components/Layout";
-import Education from "../components/Education";
-
 export default function EducationPage() {
   return (
     <Layout>
       <main className="px-6 py-12 max-w-5xl mx-auto text-white">
-        <h1 
-          className="text-3xl font-bold text-center mb-10 border-b pb-4 border-gray-600 dark:border-gray-400"
-          tabIndex={0}
-          aria-label="Education page"
-        >
-        </h1>
         <Education />
       </main>
     </Layout>
