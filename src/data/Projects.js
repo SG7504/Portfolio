d73bc1b4f28e6362bc8164f379a604429b6960c2
Sparsh Guha <143119599+SG7504@users.noreply.github.com>
2026-01-25 15:01:26 +0530

    Update Projects.js

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -7,6 +7,20 @@ const projects = [
     code: "https://github.com/SG7504/Fraud-Detection-System-with-AWS",
   },
   {
+    title: "Dragon’s Vault Discord Bot",
+    description:
+      "Built an automation-focused Discord bot with role-based access control and ticketing workflows, reducing moderator workload by 50+ tasks per month. Implemented Flask-based admin endpoints and automated over 5,000 support queries and role assignments using the Discord API.",
+    stack: ["Python", "Flask", "Discord API", "GitHub"],
+    code: "https://github.com/SG7504/Discord-Bot-DV",
+  },
+  {
+    title: "Speech-To-Text-Offline-Subtitle-Generator",
+    description:
+      "Implemented an offline SRT subtitle translation pipeline using machine learning models, preserving original timestamps and processing 200–300 lines per minute. Achieved over 85% translation accuracy and reduced multi-step manual effort by 60% without requiring internet connectivity.",
+    stack: ["Python", "Machine Learning", "NLP", "Google Colab"],
+    code: "https://github.com/SG7504/GFGKIIT-Wanderers-Hyperthon",
+  },
+    {
     title: "EEG-Based Dementia Classification System",
     description:
       "Developed an end-to-end EEG signal classification pipeline to distinguish between Healthy, Mild Cognitive Impairment (MCI), and Dementia patients. Trained and evaluated multiple models (XGBoost, LightGBM, Random Forest, SVM), achieving 94.0% accuracy and 0.94 F1-score using XGBoost, and deployed real-time inference through a FastAPI backend.",
@@ -20,20 +34,6 @@ const projects = [
       "scikit-learn",
     ],
     code: "https://github.com/SG7504/EEG-Based-Dementia-Classification-System",
-  },
-  {
-    title: "Dragon’s Vault Discord Bot",
-    description:
-      "Built an automation-focused Discord bot with role-based access control and ticketing workflows, reducing moderator workload by 50+ tasks per month. Implemented Flask-based admin endpoints and automated over 5,000 support queries and role assignments using the Discord API.",
-    stack: ["Python", "Flask", "Discord API", "GitHub"],
-    code: "https://github.com/SG7504/Discord-Bot-DV",
-  },
-  {
-    title: "Speech-To-Text-Offline-Subtitle-Generator",
-    description:
-      "Implemented an offline SRT subtitle translation pipeline using machine learning models, preserving original timestamps and processing 200–300 lines per minute. Achieved over 85% translation accuracy and reduced multi-step manual effort by 60% without requiring internet connectivity.",
-    stack: ["Python", "Machine Learning", "NLP", "Google Colab"],
-    code: "https://github.com/SG7504/GFGKIIT-Wanderers-Hyperthon",
   },
 ];
 
