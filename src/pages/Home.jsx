c22c3bb7ccae261aa073453a323f4793f77bb370
Sparsh Guha <143119599+SG7504@users.noreply.github.com>
2026-01-26 11:04:56 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -110,6 +110,11 @@ export default function Home() {
             Email copied!
           </div>
         )}
+        {/* Last Updated */}
+<p className="mt-6 text-xs text-zinc-500 text-center">
+  Last updated · January 2026
+</p>
+
       </main>
     </Layout>
   );
