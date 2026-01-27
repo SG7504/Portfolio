bbaf59533f503aaaa3cd235a6cd2c26f78caa841
Sparsh Guha <143119599+SG7504@users.noreply.github.com>
2026-01-27 16:31:58 +0530

    Update Experience.jsx

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -5,7 +5,7 @@ import ExperienceCard from "../components/ExperienceCard";
 export default function ExperiencePage() {
   return (
     <Layout>
-      <section className="min-h-screen px-6 py-12 max-w-5xl mx-auto text-white">
+      <section className="min-h-screen px-6 py-12 max-w-6xl mx-auto text-white">
         <h2 className="text-4xl font-bold mb-10 text-center">Experience</h2>
 
         <div className="space-y-8">
