e9cec15711692c11887ed824c34e2e25421152da
Sparsh Guha <143119599+SG7504@users.noreply.github.com>
2026-01-27 16:32:31 +0530

    Update Experience.jsx

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -8,7 +8,7 @@ export default function ExperiencePage() {
       <section className="min-h-screen px-6 py-12 max-w-6xl mx-auto text-white">
         <h2 className="text-4xl font-bold mb-10 text-center">Experience</h2>
 
-        <div className="space-y-8">
+        <div className="grid gap-10 grid-cols-1">
           {experience.map((exp, index) => (
             <ExperienceCard key={index} {...exp} />
           ))}
