f8ed34a3966108b911c822fba0ea74772da18482
Sparsh Guha <143119599+SG7504@users.noreply.github.com>
2026-01-27 16:34:10 +0530

    Update ExperienceCard.jsx

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -10,7 +10,7 @@ export default function ExperienceCard({
 }) {
   return (
     <motion.article
-      className="bg-zinc-800 p-6 rounded-2xl shadow-lg border border-zinc-700 relative transition-shadow"
+      className="bg-zinc-800 w-full p-6 rounded-2xl shadow-lg border border-zinc-700 relative transition-shadow"
       initial={{ opacity: 0, y: 30 }}
       whileInView={{ opacity: 1, y: 0 }}
 
